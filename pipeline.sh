540ba69681b1c9ca2f9a0c83b78eb82078cb5426
Michael Lange <dingoeatingfuzz@gmail.com>
2019-10-03 00:48:03 -0700

    Expand zoom layers to include the levels the map uses

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,4 @@
-time tl copy -z 10 -Z 14 \
+time tl copy -z 10 -Z 16 \
   -b "-123.2762 45.1926 -122.1817 45.8201" \
   "https://api.mapbox.com/v4/mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v7/{z}/{x}/{y}.vector.pbf?access_token=$MAPBOX_TOKEN" \
   mbtiles://./tiles.mbtiles

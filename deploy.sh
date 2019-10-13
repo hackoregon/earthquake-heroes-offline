9caba18732ee129d34fc93c245db626dca3e64a8
Michael Lange <dingoeatingfuzz@gmail.com>
2019-10-12 20:19:48 -0700

    Use a variable for the project name

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+NAME = "2019-earthquake-heroes-offline"
+
 echo "Setting up AWS"
 
 pip install --user awscli
@@ -9,11 +11,11 @@ eval $(aws ecr get-login --no-include-email --region us-west-2)
 
 echo "Building docker container"
 
-docker build -t earthquake-heroes-offline .
-docker tag earthquake-heroes-offline:latest $REMOTE_IMAGE_URL/earthquake-heroes-offline:latest
-docker tag earthquake-heroes-offline:latest $REMOTE_IMAGE_URL/earthquake-heroes-offline:$TRAVIS_BUILD_NUMBER
+docker build -t $NAME .
+docker tag $NAME:latest $REMOTE_IMAGE_URL/$NAME:latest
+docker tag $NAME:latest $REMOTE_IMAGE_URL/$NAME:$TRAVIS_BUILD_NUMBER
 
 echo "Pushing docker container ($TRAVIS_BUILD_NUMBER)"
 
-docker push $REMOTE_IMAGE_URL/earthquake-heroes-offline:latest
-docker push $REMOTE_IMAGE_URL/earthquake-heroes-offline:$TRAVIS_BUILD_NUMBER
+docker push $REMOTE_IMAGE_URL/$NAME:latest
+docker push $REMOTE_IMAGE_URL/$NAME:$TRAVIS_BUILD_NUMBER

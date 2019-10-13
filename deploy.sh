755efd3598b23b59967da8144820ef65669a0c1c
Michael Lange <dingoeatingfuzz@gmail.com>
2019-10-12 20:12:30 -0700

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,19 @@
-echo "TODO: Write the deploy script"
+echo "Setting up AWS"
+
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+echo "Accessing ECR"
+
+eval $(aws ecr get-login --no-include-email --region us-west-2)
+
+echo "Building docker container"
+
+docker build -t earthquake-heroes-offline .
+docker tag earthquake-heroes-offline:latest $REMOTE_IMAGE_URL/earthquake-heroes-offline:latest
+docker tag earthquake-heroes-offline:latest $REMOTE_IMAGE_URL/earthquake-heroes-offline:$TRAVIS_BUILD_NUMBER
+
+echo "Pushing docker container ($TRAVIS_BUILD_NUMBER)"
+
+docker push $REMOTE_IMAGE_URL/earthquake-heroes-offline:latest
+docker push $REMOTE_IMAGE_URL/earthquake-heroes-offline:$TRAVIS_BUILD_NUMBER
